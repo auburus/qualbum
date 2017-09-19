5d16702345c6454a87d1a9fcacb48b4c1598761d
Jordi <jordi.nopa@gmail.com>
2017-09-19 17:24:37 -0600

    Add buttons

--- qualbum.cs
+++ qualbum.cs
@@ -1,7 +1,8 @@
 using Gtk;
 
-class SharpApp : Window {
-    public SharpApp() : base("Center")
+class SharpApp : Window
+{
+    public SharpApp() : base("Buttons")
     {
         SetDefaultSize(250, 200);
         SetPosition(WindowPosition.Center);
@@ -9,7 +10,23 @@ class SharpApp : Window {
 
         DeleteEvent += new DeleteEventHandler(OnDelete);
 
-        Show();
+        Fixed fix = new Fixed();
+
+        Button btn1 = new Button("Button 1");
+        btn1.Sensitive = false;
+        Button btn2 = new Button("Button 2");
+        Button btn3 = new Button(Stock.Close);
+        Button btn4 = new Button("Button 4");
+        btn4.SetSizeRequest(80, 40);
+
+        fix.Put(btn1, 20, 30);
+        fix.Put(btn2, 100, 30);
+        fix.Put(btn3, 20, 80);
+        fix.Put(btn4, 100, 80);
+
+        Add(fix);
+
+        ShowAll();
     }
 
     public static void Main()
