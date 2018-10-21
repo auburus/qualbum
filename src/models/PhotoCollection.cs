63081d53e6e1296eabd0dcaec21308fedeb04b1c
Jordi <jordi.nopa@gmail.com>
2018-10-20 19:00:46 -0600

    Created photoCollection model

---
+++ src/models/PhotoCollection.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+/// Manages the current state of a photo collection
+class PhotoCollection
+{
+    private List<FileInfo> photos;
+    private int index;
+
+    public PhotoCollection(IEnumerable<FileInfo> photos)
+    {
+        this.photos = photos.ToList();
+        index = 0;
+    }
+
+    public int Count
+    {
+        get {
+            return photos.Count;
+        }
+    }
+
+    public FileInfo Current {
+        get {
+            if (index < 0 || index >= photos.Count)
+            {
+                return null;
+            }
+            return photos[index];
+        }
+    }
+
+    public int CurrentIndex
+    {
+        get {
+            return index;
+        }
+    }
+
+    public FileInfo First()
+    {
+        index = 0;
+        return this.Current;
+    }
+
+    public FileInfo Next()
+    {
+        index = (index + 1) % this.Count;
+        return this.Current;
+    }
+
+    public FileInfo Prev()
+    {
+        index = (index + (this.Count - 1)) % this.Count;
+        return this.Current;
+    }
+
+    public void Insert(FileInfo f)
+    {
+        photos.Insert(index, f);
+    }
+
+    public void Remove()
+    {
+        photos.RemoveAt(index);
+
+        // Update the index just in case is the last photo
+        index = (index + this.Count) % this.Count;
+    }
+}
